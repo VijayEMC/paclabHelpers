bd493f31d8f6e1447cdcb9db95b71d948e79bcbe
Kumar <kumarv83@uscskumarv83m1.corp.emc.com>
2018-01-25 21:04:10 -0800

    connection testing

--- checkDockerServices.py
+++ checkDockerServices.py
@@ -50,14 +50,15 @@ for service in services:
     labels = serviceObject["Spec"]["Labels"]
     print(labels)
     try:
-        if labels["cron"] == True:
+        if labels["cron"] == 'true':
             labelKey = "CronSchedule"
-            labelValue = labels["CronSchedule"]
+            labelValue = labels["cronSchedule"]
 
     except:
             continue
-
-    c = consul.Consul(host=consuIP, port=consulPort, scheme='https',verify=False)
+  
+    c = consul.Consul(host=consulIP, port=consulPort, scheme='https', verify=False)
     c.kv.put(labelKey, labelValue)
     alfred.info("Added {0}:{1} key-value pair to Consul".format(labelKey, labelValue))
+    
 
